1168499931e29d54b60223770d67e237eac34cf3
isabella <danna.garcia@udea.edu.co>
2023-11-17 21:26:25 -0500

    visual

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -36,7 +36,63 @@
    </div>
  </div>
 </header>   
-   <router-view></router-view>
+    <router-view></router-view>
+    <div id="carouselExampleDark" class="carousel carousel slide" style="margin-top: 100px; background-color: #003e4b;" data-bs-interval="3000">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active" data-bs-interval="10000">
+        <img src="../assets/admon.png" class="d-block" alt="banner 1" style="max-height: 300px; margin-left: 10%;" >
+        <div class="carousel-caption d-none d-md-block"  style="position: absolute; top: 0; left: 100px; right: 0; text-align: center; padding: 20px;">
+          <div style="text-align: center;">
+            <h5 style="font-size: 2em;">Administrador</h5>
+          </div>
+          <p style="text-align: justify; max-width: 600px; margin: 20px auto;">La plataforma facilita la gestión de cuentas de usuario, establecer estándares
+                de habilitación, definir los servicios que serán evaluados y proporcionar herramientas como listas
+                de chequeo para garantizar el cumplimiento de las condiciones de habilitación en la atención médica
+          </p>
+        </div>
+      </div>
+      <div class="carousel-item" data-bs-interval="2000">
+        <img src="../assets/usuario.png" class="d-block" alt="banner 2" style="max-height: 300px; margin-left: 8%;">
+        <div class="carousel-caption d-none d-md-block"  style="position: absolute; top: 0; left: 100px; right: 0; text-align: center; padding: 20px;">
+          <div style="text-align: center;">
+            <h5 style="font-size: 2em;">Usuario</h5>
+          </div>
+          <p style="text-align: justify; max-width: 600px; margin: 20px auto;">La plataforma de autoevaluación esta diseñada para monitorear y mejorar el cumplimiento 
+                de los estándares de habilitación en su organización de salud. A través de la plataforma, 
+                pueden acceder a información visual, cargar documentos relevantes y recibir orientación 
+                para garantizar que la organización cumpla con los estándares y pueda proporcionar 
+                servicios de alta calidad y seguridad.
+          </p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="../assets/auditor.png" class="d-block" alt="danner 3" style="max-height: 300px; margin-left: 10%;">
+        <div class="carousel-caption d-none d-md-block"  style="position: absolute; top: 0; left: 100px; right: 0; text-align: center; padding: 20px;">
+          <div style="text-align: center;">
+            <h5 style="font-size: 2em;">Auditor</h5>
+          </div>
+          <p style="text-align: justify; max-width: 600px; margin: 20px auto;">La plataforma permite la supervisión y evaluación de las organizaciones de salud asignadas. 
+                permite enviar recomendaciones, llevar a cabo revisiones y evaluar el cumplimiento de estándares. 
+                Permitiendo colaborar a que las organizaciones cumplan con los requisitos establecidos y mantengan altos estándares de calidad 
+                y seguridad en la atención médica que brindan.
+          </p>
+        </div>
+      </div>
+    </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
    <section class="py-5 text-center container">
      <div class="row mb-5" style="margin-top: 70px;">
          <div class="col-md-6 col-lg-4 mb-3">
@@ -124,5 +180,26 @@
 
 .input-group {
  margin-top: 10px;
+}
+.carousel-inner img {
+  max-height: 300px; /* Ajusta el valor según sea necesario */
+}
+.carousel-caption {
+    max-height: 600px; 
+  }
+  .carousel-image-container {
+  position: relative;
+  height: 200px; 
+  overflow: hidden;
+  background-color: #274e59;
+}
+
+.carousel-image-container img {
+  position: absolute;
+  top: 10%;
+  left: 10%;
+  transform: translate(-50%, -50%);
+  width: 50%;
+  height: 50%;
 }
 </style>
