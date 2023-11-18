09ea0fe2fc149340c7b120395a64d3f5d8f33276
isabella <danna.garcia@udea.edu.co>
2023-11-18 17:45:54 -0500

    homepage terminada

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <template>
   <header data-bs-theme="dark" class="fixed-top">
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
    <div class="collapse text-bg-dark" id="navbarHeader">
      <div class="container">
        <div class="row">
@@ -50,7 +52,7 @@
           <div style="text-align: center;"> 
             <h5 style="font-size: 2em;">Administrador</h5>
           </div>
-          <p style="text-align: justify; max-width: 600px; margin: 20px auto;">La plataforma facilita la gestión de cuentas de usuario, establecer estándares
+          <p style="text-align: justify; max-width: 600px; margin: 20px auto; font-size: 1.2em;">La plataforma facilita la gestión de cuentas de usuario, establecer estándares
                 de habilitación, definir los servicios que serán evaluados y proporcionar herramientas como listas
                 de chequeo para garantizar el cumplimiento de las condiciones de habilitación en la atención médica.
           </p>
@@ -62,7 +64,7 @@
           <div style="text-align: center;">
             <h5 style="font-size: 2em;">Usuario</h5>
           </div>
-          <p style="text-align: justify; max-width: 600px; margin: 20px auto;">La plataforma de autoevaluación esta diseñada para monitorear y mejorar el cumplimiento 
+          <p style="text-align: justify; max-width: 600px; margin: 20px auto;  font-size: 1.2em">La plataforma de autoevaluación esta diseñada para monitorear y mejorar el cumplimiento 
                 de los estándares de habilitación en su organización de salud. A través de la plataforma, 
                 pueden acceder a información visual, cargar documentos relevantes y recibir orientación 
                 para garantizar que la organización cumpla con los estándares y pueda proporcionar 
@@ -76,7 +78,7 @@
           <div style="text-align: center;">
             <h5 style="font-size: 2em;">Auditor</h5>
           </div>
-          <p style="text-align: justify; max-width: 600px; margin: 20px auto;">La plataforma permite la supervisión y evaluación de las organizaciones de salud asignadas. 
+          <p style="text-align: justify; max-width: 600px; margin: 20px auto;  font-size: 1.2em">La plataforma permite la supervisión y evaluación de las organizaciones de salud asignadas. 
                 permite enviar recomendaciones, llevar a cabo revisiones y evaluar el cumplimiento de estándares. 
                 Permitiendo colaborar a que las organizaciones cumplan con los requisitos establecidos y mantengan altos estándares de calidad 
                 y seguridad en la atención médica que brindan.
@@ -94,25 +96,26 @@
   </button>
 </div>
     <section class="py-5 text-center container">
-      <div class="row py-lg-5">
+      <div class="row py-lg-5" data-aos="fade-down">
         <div class="heading_container heading_center">
           <h5 style="font-size: 5em; font-weight: bold;">Resolución 3100 de 2019</h5>
         </div>
-        <p>Por la cual se definen los procedimientos y condiciones de inscripción de los prestadores
+        <p style="font-size: 1.2em;">Por la cual se definen los procedimientos y condiciones de inscripción de los prestadores
           de servicios de salud y de habilitación de los servicios de salud y se adopta el Manual de 
           Inscripción de Prestadores y Habilitación de Servicios de Salud.
         </p>
       </div>
     </section>
-    <section class="headers1 cid-svGO3o7YHJ" id="header01-0" style="background-color: #f0f0f0;">
-        <div class="container">
+    <section class="headers1 cid-svGO3o7YHJ" id="header01-0" style="background-color: #f0f0f0;" >
+        <div class="container" data-aos="fade-right">
             <div class="row align-items-center">
                 <div class="col-12 col-md-12 col-lg-5 title-col">
                     <h6 class="mbr-section-subtitle align-left mbr-fonts-style my-3 display-1"><strong>Servicios</strong></h6>
+                    <img src="../assets/servicios.png" class="d-block" alt="Intento 1" style="width: 50%; max-width: 400px; margin-left: 10%;">
                 </div>
                 <div class="col-12 col-md-12 text-col col-lg-6">
-                    <p class="mbr-text align-left mbr-fonts-style mb-0 display-7" style="margin-top: 4%;">Servicios con los que cuentan las entidades de salud:<br></p>
-                    <ol class="list-group-numbered" style="text-align: justify; padding-left: 10%; margin-top: 2%;">
+                    <p class="mbr-text align-left mbr-fonts-style mb-0 display-7" style="margin-top: 4%; font-size: 1.2em;">Servicios con los que cuentan las entidades de salud:<br></p>
+                    <ol class="list-group-numbered" style="text-align: justify; padding-left: 10%; margin-top: 2%; font-size: 1.2em;">
                         <li class="list-group-item">Consulta externa.</li>
                         <li class="list-group-item">Servicio farmaceutico.</li>
                         <li class="list-group-item">Servicio de dialisis.</li>
@@ -127,14 +130,15 @@
         </div>
     </section>
     <section class="headers1 cid-svGO3o7YHJ" id="header01-0" style="background-color: white;">
-        <div class="container">
+        <div class="container" data-aos="fade-left">
             <div class="row align-items-center">
                 <div class="col-12 col-md-12 col-lg-5 title-col">
                     <h6 class="mbr-section-subtitle align-left mbr-fonts-style my-3 display-1"><strong>Estándar</strong></h6>
+                    <img src="../assets/estandar.png" class="d-block" alt="Intento 1" style="width: 50%; max-width: 400px; margin-left: 10%;">
                 </div>
                 <div class="col-12 col-md-12 text-col col-lg-6">
-                    <p class="mbr-text align-left mbr-fonts-style mb-0 display-7" style="margin-top: 4%;">Se presentan los estándares con los que cada servicio cuenta:<br></p>
-                    <ol class="list-group-numbered" style="text-align: justify; padding-left: 10%; margin-top: 2%;">
+                    <p class="mbr-text align-left mbr-fonts-style mb-0 display-7" style="margin-top: 4%; font-size: 1.2em;">Se presentan los estándares con los que cada servicio cuenta:<br></p>
+                    <ol class="list-group-numbered" style="text-align: justify; padding-left: 10%; margin-top: 2%; font-size: 1.2em;">
                         <li class="list-group-item">Talento humano.</li>
                         <li class="list-group-item">Infraestructura.</li>
                         <li class="list-group-item">Dotación.</li>
@@ -148,13 +152,14 @@
         </div>
     </section>
     <section class="headers1 cid-svGO3o7YHJ" id="header01-0" style="background-color: #f0f0f0; padding-bottom: 30px;">
-        <div class="container">
+        <div class="container" data-aos="fade-right">
             <div class="row align-items-center">
-                <div class="col-12 col-md-12 col-lg-5 title-col" style="text-align: justify;">
+                <div class="col-12 col-md-12 col-lg-5 title-col">
                     <h6 class="mbr-section-subtitle align-left mbr-fonts-style my-3 display-1"><strong>Criterios</strong></h6>
+                    <img src="../assets/criterios.png" class="d-block" alt="Intento 1" style="width: 50%; max-width: 400px; margin-left: 10%;">
                 </div>
-                <div class="col-12 col-md-12 text-col col-lg-6">
-                    <p class="mbr-text align-left mbr-fonts-style mb-0 display-7" style="margin-top: 4%;">Los criterios son los preceptos o indicadores
+                <div class="col-12 col-md-12 text-col col-lg-6" style="text-align: justify;" text-align="justify">
+                    <p class="mbr-text align-left mbr-fonts-style mb-0 display-7" style="margin-top: 4%; font-size: 1.2em;">Los criterios son los preceptos o indicadores
                       obligatorios que resumen la calidad de una actividad que se pretende evaluar. Para efectos de la presente webpage, el criterio es la unidad
                       básica del estándar sobre el cual se realiza la verificación orientada hacia el análisis
                       de los riesgos y la seguridad en la atención de los pacientes.<br></p>
@@ -163,7 +168,7 @@
         </div>
     </section>
      <section class="py-5 text-center container">
-       <div class="row py-lg-5">
+       <div class="row py-lg-5" data-aos="flip-down">
          <div class="col-lg-6 col-md-8 mx-auto">
            <h1 class="fw-light">Quienes somos</h1>
            <p class="lead text-body-secondary">Fundada con la visión de fusionar la biotecnología y el código, nuestra empresa se ha convertido en un referente en el mundo del desarrollo web. Nuestro equipo de expertos en diseño, programación y marketing digital trabaja incansablemente para transformar ideas en realidades digitales impactantes. Desde sitios web a medida hasta aplicaciones interactivas, en bioCode Innovators, creamos soluciones web que no solo impresionan visualmente, sino que también impulsan el éxito de nuestros clientes en el mundo en línea. ¡Únete a nosotros en este emocionante viaje hacia un futuro digital más brillante!</p>
@@ -181,6 +186,8 @@
   </div>
 </template>
 <script>
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 export default {
   mounted() {
     const navbarHeader = document.getElementById('navbarHeader');
@@ -195,6 +202,7 @@ export default {
       // Restaurar el margen superior del carrusel cuando se oculta el menú
       carouselContainer.style.marginTop = '100px'; // Ajusta según sea necesario
     });
+    AOS.init();
   }
 }
 </script>
@@ -259,4 +267,7 @@ export default {
   margin-top: 100px; 
   transition: margin-top 0.5s ease; 
 }
+body {
+    font-family: 'Roboto', sans-serif;
+  }
 </style>
