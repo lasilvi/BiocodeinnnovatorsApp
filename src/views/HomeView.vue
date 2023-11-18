9d1e683c7a1cc28de188de0533345aa85524b930
isabella <danna.garcia@udea.edu.co>
2023-11-18 13:12:22 -0500

    visual

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -93,55 +93,75 @@
     <span class="visually-hidden">Next</span>
   </button>
 </div>
-   <section class="py-5 text-center container">
-     <div class="row mb-5" style="margin-top: 70px;">
-         <div class="col-md-6 col-lg-4 mb-3">
-           <div class="card h-100">
-            <img class="card-img-top" src="../assets/admon.png" alt="Card image cap" style="max-height: 300px;">
-             <div class="card-body">
-               <h5 class="card-title">Administrador</h5>
-               <p class="card-text">
-                La plataforma facilita la gestión de cuentas de usuario, establecer estándares
-                de habilitación, definir los servicios que serán evaluados y proporcionar herramientas como listas
-                de chequeo para garantizar el cumplimiento de las condiciones de habilitación en la atención médica
-               </p>
-             
-             </div>
-           </div>
-         </div>
-         <div class="col-md-6 col-lg-4 mb-3">
-           <div class="card h-100">
-            <img class="card-img-top" src="../assets/usuario.png" alt="Card image cap" style="max-height: 300px;">
-             <div class="card-body">
-               <h5 class="card-title">Usuario</h5>
-               <p class="card-text">
-                La plataforma de autoevaluación esta diseñada para monitorear y mejorar el cumplimiento 
-                de los estándares de habilitación en su organización de salud. A través de la plataforma, 
-                pueden acceder a información visual, cargar documentos relevantes y recibir orientación 
-                para garantizar que la organización cumpla con los estándares y pueda proporcionar 
-                servicios de alta calidad y seguridad.
-               </p>
-               
-             </div>
-           </div>
-         </div>
-         <div class="col-md-6 col-lg-4 mb-3">
-           <div class="card h-100">
-            <img class="card-img-top" src="../assets/auditor.png" alt="Card image cap"  style="max-height: 300px;">
-             <div class="card-body">
-               <h5 class="card-title">Auditor</h5>
-               <p class="card-text">
-                La plataforma permite la supervisión y evaluación de las organizaciones de salud asignadas. 
-                permite enviar recomendaciones, llevar a cabo revisiones y evaluar el cumplimiento de estándares. 
-                Permitiendo colaborar a que las organizaciones cumplan con los requisitos establecidos y mantengan altos estándares de calidad 
-                y seguridad en la atención médica que brindan.
-               </p>
-            
-             </div>
-           </div>
-         </div>
-       </div>
-     </section>
+    <section class="py-5 text-center container">
+      <div class="row py-lg-5">
+        <div class="heading_container heading_center">
+          <h5 style="font-size: 5em; font-weight: bold;">Resolución 3100 de 2019</h5>
+        </div>
+        <p>Por la cual se definen los procedimientos y condiciones de inscripción de los prestadores
+          de servicios de salud y de habilitación de los servicios de salud y se adopta el Manual de 
+          Inscripción de Prestadores y Habilitación de Servicios de Salud.
+        </p>
+      </div>
+    </section>
+    <section class="headers1 cid-svGO3o7YHJ" id="header01-0" style="background-color: #f0f0f0;">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-12 col-md-12 col-lg-5 title-col">
+                    <h6 class="mbr-section-subtitle align-left mbr-fonts-style my-3 display-1"><strong>Servicios</strong></h6>
+                </div>
+                <div class="col-12 col-md-12 text-col col-lg-6">
+                    <p class="mbr-text align-left mbr-fonts-style mb-0 display-7" style="margin-top: 4%;">Servicios con los que cuentan las entidades de salud:<br></p>
+                    <ol class="list-group-numbered" style="text-align: justify; padding-left: 10%; margin-top: 2%;">
+                        <li class="list-group-item">Consulta externa.</li>
+                        <li class="list-group-item">Servicio farmaceutico.</li>
+                        <li class="list-group-item">Servicio de dialisis.</li>
+                        <li class="list-group-item"><strong>Servicio de cirugía.</strong></li>
+                        <li class="list-group-item">Servicio de hospitalización.</li>
+                        <li class="list-group-item">Servicio de imágenes diagnósticas.</li>
+                        <li class="list-group-item">Servicio de transporte clínico.</li>
+                        <li class="list-group-item">Servicio de patología.</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="headers1 cid-svGO3o7YHJ" id="header01-0" style="background-color: white;">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-12 col-md-12 col-lg-5 title-col">
+                    <h6 class="mbr-section-subtitle align-left mbr-fonts-style my-3 display-1"><strong>Estándar</strong></h6>
+                </div>
+                <div class="col-12 col-md-12 text-col col-lg-6">
+                    <p class="mbr-text align-left mbr-fonts-style mb-0 display-7" style="margin-top: 4%;">Se presentan los estándares con los que cada servicio cuenta:<br></p>
+                    <ol class="list-group-numbered" style="text-align: justify; padding-left: 10%; margin-top: 2%;">
+                        <li class="list-group-item">Talento humano.</li>
+                        <li class="list-group-item">Infraestructura.</li>
+                        <li class="list-group-item">Dotación.</li>
+                        <li class="list-group-item">Medicamentos, dispositivos médicos e insumos.</li>
+                        <li class="list-group-item">Procesos prioritarios.</li>
+                        <li class="list-group-item">Hisotia clínica y registros.</li>
+                        <li class="list-group-item">Interdependencia.</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="headers1 cid-svGO3o7YHJ" id="header01-0" style="background-color: #f0f0f0; padding-bottom: 30px;">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-12 col-md-12 col-lg-5 title-col" style="text-align: justify;">
+                    <h6 class="mbr-section-subtitle align-left mbr-fonts-style my-3 display-1"><strong>Criterios</strong></h6>
+                </div>
+                <div class="col-12 col-md-12 text-col col-lg-6">
+                    <p class="mbr-text align-left mbr-fonts-style mb-0 display-7" style="margin-top: 4%;">Los criterios son los preceptos o indicadores
+                      obligatorios que resumen la calidad de una actividad que se pretende evaluar. Para efectos de la presente webpage, el criterio es la unidad
+                      básica del estándar sobre el cual se realiza la verificación orientada hacia el análisis
+                      de los riesgos y la seguridad en la atención de los pacientes.<br></p>
+                </div>
+            </div>
+        </div>
+    </section>
      <section class="py-5 text-center container">
        <div class="row py-lg-5">
          <div class="col-lg-6 col-md-8 mx-auto">
@@ -190,7 +210,6 @@ export default {
  left: 0; /* Fija el sidebar al lado izquierdo */
  top: 0;
  height: 100%; /* Haz que el sidebar ocupe toda la altura de la pantalla */
- /* Resto de los estilos... */
 }
 
 .sidebar-header {
@@ -206,7 +225,7 @@ export default {
  margin-top: 10px;
 }
 .carousel-inner img {
-  max-height: 300px; /* Ajusta el valor según sea necesario */
+  max-height: 300px; 
 }
 .carousel-caption {
     max-height: 600px; 
@@ -227,17 +246,17 @@ export default {
   height: 50%;
 }
 .footer {
-  background-color: #003e4b; /* Cambia el color del footer según tus necesidades */
+  background-color: #003e4b; 
   text-decoration: none; /* Quita la subrayado del enlace */
   width: 100%;
 }
 
 .footer a {
-  color: white; /* Cambia el color del texto del enlace a blanco */
+  color: white; 
 }
 
 #carouselExampleDark {
-  margin-top: 100px; /* ajusta según sea necesario */
-  transition: margin-top 0.5s ease; /* agrega transición para suavizar el cambio */
+  margin-top: 100px; 
+  transition: margin-top 0.5s ease; 
 }
 </style>
